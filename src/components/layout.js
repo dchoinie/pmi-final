d91abf32f4c5d60bef2a67de88d0fc3b5e485fbf
Dan Choiniere <dchoinie@gmail.com>
2020-11-20 09:07:54 -0600

    Update layout.js
    padding on covid alert mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import Footer from "./footer"
 const Layout = ({ children }) => {
   return (
     <>
-    <div className="flex justify-center bg-red-600 py-2">
+    <div className="flex justify-center bg-red-600 py-2 px-6 lg:px-0">
       <p className="text-white">
         Due to COVID-19 restrictions, PMI is temporarily closed. To schedule a workout appointment visit our <a href="https://www.facebook.com/pmitotalfitness/appointments" className="text-blue-400 underline" target="_blank" rel="noopener noreferrer">Facebook Page</a>
       </p>
