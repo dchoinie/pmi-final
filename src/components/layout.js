c1d1c75c72c58eef62e71ea9d7c369f56e6b78ef
Dan Choiniere <dchoinie@gmail.com>
2021-01-05 14:59:39 -0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,11 +6,6 @@ import Footer from "./footer"
 const Layout = ({ children }) => {
   return (
     <>
-//     <div className="flex justify-center bg-red-600 py-2 px-6 lg:px-0">
-//       <p className="text-white">
-//         Due to COVID-19 restrictions, PMI is temporarily closed. To schedule a workout appointment visit our <a href="https://www.facebook.com/pmitotalfitness/appointments" className="text-blue-400 underline" target="_blank" rel="noopener noreferrer">Facebook Page</a>
-//       </p>
-//     </div>
       <NavLarge />
       <NavSmall />
       <main>{children}</main>
